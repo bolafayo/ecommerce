f154abf9136c26650ab19f1ce7dd383416744caf
Bolade Olufayo Taiwo <olufayobolade@gmail.com>
2025-07-20 19:40:10 +0100

    created a function for the registration page

--- registration.js
+++ registration.js
@@ -16,24 +16,34 @@ async function handleSubmit(e) {
 
   const formData = new FormData(form)
   const name = formData.get("name")
-  const username = formData.get("userName")
+  const username = formData.get("username")
   const password = formData.get("password")
   const email = formData.get("email")
 
   if (name === '' || username === '' || password === '' || email === '') {
     console.log("Please fill every input box");
-  } else {
+     info.innerHTML = `<p style="color:red;">Please fill every input box</p>`;
+    return;
+    
+  } 
     const user = {
       username,
       password,
-      email
+      email,
+       name: {
+    firstname: name.split(" ")[0] || "",
+    lastname: name.split(" ")[1] || ""
+  },
     }
     try {
       const response = await createUser(user);
+       const userData = await response.json()
+        console.log(user)
+        
       if (response.ok) {
-        const userData = await response.json()
-        console.log("user created" , userData)
-         info.innerHTML = `<p>User Created: ${userData.username} </p>`;
+        storedUsers.push(user);
+        localStorage.setItem('users', JSON.stringify(storedUsers));
+       
           form.reset();
          
             } else {
@@ -44,15 +54,9 @@ async function handleSubmit(e) {
     } catch (error) {
       console.error("Error creating user:", error);
        info.innerHTML = `<p style="color:red;">Something went wrong</p>`;
-    }
-
-    
+    } 
   }
-  
-}
-
 submitBtn.addEventListener('click', (e) => handleSubmit(e))
-
 let storedUsers = [];
 try {
   const usersFromStorage = localStorage.getItem('users');
@@ -65,21 +69,17 @@ try {
   info.innerHTML = '';
   for (let i = 0; i < storedUsers.length; i++) {
   const user = storedUsers[i];
-  console.log(user);
-
-  const content = `<div>
-  <p>${user.name}</p>
-  <p>${user.username}</p>
-  <p>${user.email}</p>
+const content=` <div>
+ <p>Name: ${user.name.firstname}</p>
+  <p>Username:${user.username}</p>
+  <p>Email: ${user.email}</p>
   </div>`;
-
+  console.log(user);
   info.innerHTML += content;
 }
-if (storedUsers.length > 0) {
-  info.innerHTML = '';
-  storedUsers.forEach(user => {
-   info.innerHTML += `<div><p>${user.name}</p></div>`;
-});
 
-}
+
+
+
+
 
